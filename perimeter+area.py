3c2569e3a1445e06c6e59de6a05c95d047472cf4
hadja1307 <56490321+hadja1307@users.noreply.github.com>
2019-12-27 22:40:55 +0000

    Add files via upload

---
+++ perimeter+area.py
@@ -0,0 +1,8 @@
+height = 25
+width = 1.65
+area = height * width
+perimeter = (height + width) * 2
+
+print("the area of the rectangle is,", area)
+
+print("the perimeter of the rectangle is,", perimeter)
